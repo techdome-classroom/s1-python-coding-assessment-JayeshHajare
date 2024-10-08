e5e4678f66ab9bbe0e68f3731c2db8642f7e856a
JayeshHajare <161421745+JayeshHajare@users.noreply.github.com>
2024-10-08 12:58:35 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,16 @@
 def decode_message( s: str, p: str) -> bool:
-
-# write your code here
-  
-        return False
+  memo = {}
+  def dp(i, j):
+    if (i, j) in memo:
+      return memo[(i, j)]
+    if j == len(p):
+      return i == len(s)
+    if p[j] == '*':
+      memo[(i, j)] = (dp(i, j + 1) or (i < len(s) and dp(i + 1, j)))
+      return memo[(i, j)]
+    if i < len(s) and (p[j] == '?' or p[j] == s[i]):
+      memo[(i, j)] = dp(i + 1, j + 1)
+      return memo[(i, j)]
+    memo[(i, j)] = False
+    return False
+  return dp(0, 0)
